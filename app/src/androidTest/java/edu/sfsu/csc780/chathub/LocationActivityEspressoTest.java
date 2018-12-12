251ce9eb02f6032d8204213bc4e6c41fb40daa8c
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-12-12 12:48:49 -0800

    espresso test for location added

---
+++ app/src/androidTest/java/edu/sfsu/csc780/chathub/LocationActivityEspressoTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.sfsu.csc780.chathub;
+
+import android.os.SystemClock;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.sfsu.csc780.chathub.ui.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class LocationActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureLocationSend() {
+        onView(withId(R.id.locationButton))
+                .perform(click());
+
+        SystemClock.sleep(2500);
+
+        onView(withId(R.id.locationButton)).check(matches(isDisplayed()));
+    }
+}
